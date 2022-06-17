df88f0e18be0c8daede27b5124bd60dc6e815372
shraddhabaheti <shraddhabaheti123@gmail.com>
2022-06-17 17:12:11 +0530

    nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,27 +2,31 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 function Navbars() {
-  return (
-    <div>
 
-      <Navbar bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home"></Navbar.Brand>
-          <Nav className="me-auto">
-          <Nav.Link href="/">Registration</Nav.Link>
-          <Nav.Link href="/login">Login</Nav.Link>
-        <Nav.Link href="/home">Home</Nav.Link>
-       <Nav.Link href="/reg">classRegistration</Nav.Link>
-       <Nav.Link href="/loginc">classLogin</Nav.Link>
-       <Nav.Link href="/homes">classHome</Nav.Link>
+    return (
+        <div>
 
-          </Nav>
-        </Container>
-      </Navbar>
- </div>
-  )
+            <Navbar bg="primary" variant="dark" >
+                <Container>
+                    <Navbar.Brand href="#home" ></Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Link to="/" className='text-light text-decoration-none m-3'>Registration</Link>
+                        <Link to="/login" className='text-white text-decoration-none m-3'>Login</Link>
+                        <Link to="/home" className='text-white text-decoration-none m-3'>Home</Link>
+                        <Link to="/reg" className='text-white text-decoration-none m-3' >classRegistration</Link>
+                        <Link to="/loginc" className='text-white text-decoration-none m-3'>classLogin</Link>
+                        <Link to="/homes" className='text-white text-decoration-none m-3'>classHome</Link>
+
+
+
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
+    )
 }
 
 export default Navbars;
